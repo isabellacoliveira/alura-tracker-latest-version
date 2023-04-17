2ddd8711af88bba3114d83f494a7718b7c850909
Isabella Cruz de Oliveira <isaaholiveira15@gmail.com>
2023-04-17 16:16:43 -0300

    finalizando o projeto vue

---
+++ src/components/Modal.vue
@@ -0,0 +1,31 @@
+<template>
+          <!-- so vai existir se a tarefa selecionada for verdadeira = v-if -->
+          <div class="modal" :class="{'is-active' : mostrar}" v-if="mostrar">
+            <div class="modal-background"></div>
+            <div class="modal-card">
+                <!-- representa todos os elementos que estiverem dentro do modal -->
+                <header>
+                    <slot name="cabecalho" />
+                </header>
+                <section>
+                    <slot name="corpo" />
+                </section>
+                <footer>
+                    <slot name="rodape" />
+                </footer>
+            </div>
+          </div>
+</template>
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'Modal', 
+        props: {
+            mostrar: {
+                Type: Boolean, 
+                required: true
+            }
+        }
+    })
+</script>
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,40 +1,30 @@
-import { INotificacao } from "@/interfaces/INotificacao";
-import { InjectionKey } from "@vue/runtime-core";
 import { createStore, Store, useStore as vuexUseStore } from "vuex";
-import { ADICIONA_TAREFA, DEFINIR_TAREFAS, NOTIFICAR } from "./tipoMutacoes";
-import { ALTERAR_TAREFA, CADASTRAR_TAREFA, OBTER_TAREFAS } from "./tipoAcoes";
-import http from "@/http";
-import ITarefa from "@/interfaces/ITarefa";
+import { InjectionKey } from 'vue'
+import { NOTIFICAR } from "./tipoMutacoes";
+import { INotificacao } from "@/interfaces/INotificacao";
+
 import { EstadoProjeto, projeto } from "./modules/projeto";
+import { EstadoTarefa, tarefa } from "./modules/tarefas";
 
 export interface Estado {
-    tarefas: ITarefa[], 
-    notificacoes: INotificacao[], 
-    projeto: EstadoProjeto
+    notificacoes: INotificacao[],
+    projeto: EstadoProjeto,
+    tarefa: EstadoTarefa
 }
 
-export const key: InjectionKey<Store<Estado>> = Symbol();
+export const key: InjectionKey<Store<Estado>> = Symbol()
 
 export const store = createStore<Estado>({
     state: {
-        tarefas: [], 
         notificacoes: [],
-       projeto: {
+        projeto: {
             projetos: []
-       }
-    }, 
+        },
+        tarefa: {
+            tarefas: []
+        }
+    },
     mutations: {
-    
-        [DEFINIR_TAREFAS](state, tarefas: ITarefa[]){
-            state.tarefas = tarefas
-        }, 
-        [ADICIONA_TAREFA](state, tarefa: ITarefa){
-            state.tarefas.push(tarefa)
-        }, 
-        [ALTERAR_TAREFA](state, tarefa: ITarefa){
-            const index = state.tarefas.findIndex(proj => proj.id == tarefa.id)
-            state.tarefas[index] = tarefa
-        }, 
         [NOTIFICAR] (state, novaNotificacao: INotificacao) {
 
             novaNotificacao.id = new Date().getTime()
@@ -44,34 +34,13 @@ export const store = createStore<Estado>({
                 state.notificacoes = state.notificacoes.filter(notificacao => notificacao.id != novaNotificacao.id)
             }, 3000)
         }
-
     },
-    actions: {
-        [OBTER_TAREFAS] ({ commit }, filtro: string) {
-            let url = 'tarefas'
-            if(filtro){
-                url += '?descricao=' + filtro
-            }
-            http.get('tarefas')
-            .then(resposta => commit(DEFINIR_TAREFAS, resposta.data)); 
-        },
-        [CADASTRAR_TAREFA] ({ commit }, tarefa: ITarefa) {
-            return http.post('/tarefas', tarefa)
-            // para nao precisarmos ficar recarregando o estado, vamos pegar a tarefa e adicionar
-            // em nosso estado 
-            // ao cadastrar a tarefa o back devolve um objeto com as tarefas que acabamos de cadastrar        
-            .then(resposta => commit(ADICIONA_TAREFA, resposta.data))
-        }, 
-        [ALTERAR_TAREFA] ({ commit }, tarefa: ITarefa) {
-            return http.put(`/tarefas/${tarefa.id}`, tarefa)
-                .then(() => commit(ALTERAR_TAREFA, tarefa))
-        },
-    }, 
     modules: {
-        projeto
+        projeto,
+        tarefa
     }
 })
 
-export function useStore(): Store<Estado>{
+export function useStore(): Store<Estado> {
     return vuexUseStore(key)
 }
